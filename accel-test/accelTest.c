622b1d3fef5c9276bfa670b469017bafa5085802
Dave March <dbmarch@gmail.com>
2019-07-04 08:18:34 -0400

    handle some args so we can test out the various modes of the device

--- accel-test/accelTest.c
+++ accel-test/accelTest.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #include <errno.h>
 #include <string.h>
@@ -16,25 +17,19 @@
 #include "lis2dh12_reg.h"
 
 
-
-
-//#include "gpio.h"
-//#include "i2c.h"
-
 /* Private macro -------------------------------------------------------------*/
 #define DEV_I2C  "/dev/i2c-1"
 #define ACCEL_ADDR 0x19
 #define ACCEL_ADDR_2 0x18
 
-
-
 /* Private variables ---------------------------------------------------------*/
-static axis3bit16_t data_raw_acceleration;
-static axis1bit16_t data_raw_temperature;
-static float acceleration_mg[3];
-static float temperature_degC;
 static uint8_t whoamI;
 static uint8_t tx_buffer[1000];
+static bool verboseMode = false;
+static bool temperatureMode = false;
+static bool accelMode = false;
+static bool doubleTap = false;
+static uint8_t devAddr = ACCEL_ADDR;
 
 /* Private functions ---------------------------------------------------------*/
 static int32_t platform_write(void *handle, uint8_t reg, uint8_t *bufp,
@@ -42,7 +37,323 @@ static int32_t platform_write(void *handle, uint8_t reg, uint8_t *bufp,
 static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
                              uint16_t len);
 static void tx_com(uint8_t *tx_buffer, uint16_t len);
-static int platform_init(int devId);
+static int platform_init(char* devPath);
+static int get_i2c_register(int fd,
+                            unsigned char addr,
+                            unsigned char reg,
+                            unsigned char *val);
+static int set_i2c_register (int fd,
+                            unsigned char addr,
+                            unsigned char reg,
+                            unsigned char val);
+
+static void initAccelerometer( lis2dh12_ctx_t dev_ctx );
+static void readTemperature( lis2dh12_ctx_t dev_ctx ) ;
+static void readAccelerometer(lis2dh12_ctx_t dev_ctx ) ;
+
+static void usage(void) {
+  printf("d </dev/i2c-0 path to the i2c device\n");
+  printf("a <0x18,0x19> address\n");
+  printf("v             verbose Mode\n");
+  printf("t             temp Mode\n");
+  printf("x             accel Mode\n");
+  printf("p             doubleTap mode\n");
+  printf("h             this menu\n");
+  exit(0);
+}
+
+int main(int argc, char **argv)
+{
+  /*
+   *  Initialize mems driver interface
+   */
+  	
+   int opt;
+   lis2dh12_ctx_t dev_ctx;
+   char devPath[20];
+   int fd;
+   int x;
+   
+   strcpy (devPath, DEV_I2C);
+
+
+  opterr = 0;
+
+  while ((opt = getopt (argc, argv, "d:a:vtxph")) != -1)
+    switch (opt)
+      {
+        case 'd':
+          strcpy (devPath, optarg);
+          printf("devPath: %s\n", devPath);
+          break;
+        
+        case 'a':
+          sscanf(optarg, "%d",&x);
+          if (x != 0) 
+            devAddr = x;
+          else {
+            sscanf(optarg, "%x",&x);
+            if (x != 0) 
+               devAddr = x;
+          }
+          printf("devAddr = %02X\n", devAddr);
+          break;
+          
+        case 'v':
+          printf ("Verbose Mode\n");
+          verboseMode = true;
+          break;
+        case 't':
+          printf ("Temperature Mode\n");
+          temperatureMode = true;
+          break;
+        
+        case 'x': 
+          printf ("Accelerometer Mode\n");
+          accelMode = true;
+          break;
+        
+        case 'p':
+          printf("DoubleTap Mode \n");
+          doubleTap = true;
+          break;
+          
+        case 'h':
+          usage();
+          break;
+          
+        default:
+          printf ("unknown option %c\n", opt);
+          break;
+      }
+    
+  dev_ctx.write_reg = platform_write;
+  dev_ctx.read_reg = platform_read;
+  fd = platform_init(devPath);
+  if (fd < 0)
+  {
+    printf("unable to open I2C - exit\n");
+    return (-1);
+  }
+  dev_ctx.handle = (void*) fd;
+  initAccelerometer(dev_ctx);
+
+  
+  if (!temperatureMode && !accelMode && !doubleTap) {
+    printf("No mode selected....\n");
+    usage();
+  }
+  /*
+   * Read samples in polling mode (no int)
+   */
+  while (1)
+  {
+
+    if (accelMode) {
+      readAccelerometer(dev_ctx);
+    }
+
+    if (temperatureMode) {
+      readTemperature(dev_ctx);
+    }
+    sleep(1);
+  }
+  return (0);
+}
+
+
+static void initAccelerometer( lis2dh12_ctx_t dev_ctx ) {
+  
+  lis2dh12_device_id_get(&dev_ctx, &whoamI);
+
+  if (verboseMode) {
+    printf("device id: %02X\n", whoamI);  
+  }
+  if (whoamI != LIS2DH12_ID) {
+    printf ("Device not found\n");
+  } else {
+    printf ("Identifed LIS2DH12...\n");
+  }
+ 
+
+  /*
+   *  Enable Block Data Update
+   */
+  lis2dh12_block_data_update_set(&dev_ctx, PROPERTY_ENABLE);
+
+  /*
+   * Set Output Data Rate to 1Hz
+   */
+  //lis2dh12_data_rate_set(&dev_ctx, LIS2DH12_ODR_1Hz);
+  lis2dh12_data_rate_set(&dev_ctx, LIS2DH12_ODR_5kHz376_LP_1kHz344_NM_HP);
+  
+
+  /*
+   * Set full scale to 2g
+   */
+  lis2dh12_full_scale_set(&dev_ctx, LIS2DH12_2g);
+
+  /*
+   * Enable temperature sensor
+   */
+  lis2dh12_temperature_meas_set(&dev_ctx, LIS2DH12_TEMP_ENABLE);
+
+  /*
+   * Set device in continuous mode with 12 bit resol.
+   */
+  //lis2dh12_operating_mode_set(&dev_ctx, LIS2DH12_LP_8bit);
+  lis2dh12_operating_mode_set(&dev_ctx, LIS2DH12_HR_12bit);
+}
+
+static void readTemperature( lis2dh12_ctx_t dev_ctx ) {
+    lis2dh12_reg_t reg;
+    axis1bit16_t data_raw_temperature;
+    float temperature_degC;
+    
+    
+    lis2dh12_temp_data_ready_get(&dev_ctx, &reg.byte);
+    if (reg.byte)
+    {
+      /* Read temperature data */
+      memset(data_raw_temperature.u8bit, 0x00, sizeof(int16_t));
+      lis2dh12_temperature_raw_get(&dev_ctx, data_raw_temperature.u8bit);
+      temperature_degC =
+          lis2dh12_from_lsb_hr_to_celsius(data_raw_temperature.i16bit);
+
+      sprintf((char *)tx_buffer,
+              "Temperature [degC]:%6.2f\n",
+              temperature_degC);
+      tx_com(tx_buffer, strlen((char const *)tx_buffer));
+    }
+  
+}
+
+static void readAccelerometer ( lis2dh12_ctx_t dev_ctx ) {
+    lis2dh12_reg_t reg;
+    axis3bit16_t data_raw_acceleration;
+    float acceleration_mg[3];
+
+        /*
+     * Read output only if new value available
+     */
+    lis2dh12_xl_data_ready_get(&dev_ctx, &reg.byte);
+    if (reg.byte)
+    {
+      /* Read accelerometer data */
+      memset(data_raw_acceleration.u8bit, 0x00, 3 * sizeof(int16_t));
+      lis2dh12_acceleration_raw_get(&dev_ctx, data_raw_acceleration.u8bit);
+      acceleration_mg[0] =
+          lis2dh12_from_fs2_hr_to_mg(data_raw_acceleration.i16bit[0]);
+      acceleration_mg[1] =
+          lis2dh12_from_fs2_hr_to_mg(data_raw_acceleration.i16bit[1]);
+      acceleration_mg[2] =
+          lis2dh12_from_fs2_hr_to_mg(data_raw_acceleration.i16bit[2]);
+
+      sprintf((char *)tx_buffer, "Acceleration [mg]:%4.2f\t%4.2f\t%4.2f\n",
+              acceleration_mg[0], acceleration_mg[1], acceleration_mg[2]);
+      tx_com(tx_buffer, strlen((char const *)tx_buffer));
+    }
+
+  }
+
+
+
+/*
+ * @brief  Write generic device register (platform dependent)
+ *
+ * @param  handle    customizable argument. In this examples is used in
+ *                   order to select the correct sensor bus handler.
+ * @param  reg       register to write
+ * @param  bufp      pointer to data to write in register reg
+ * @param  len       number of consecutive register to write
+ *
+ */
+static int32_t platform_write(void *handle, uint8_t reg, uint8_t *bufp,
+                              uint16_t len)
+{
+  int fd = (int) handle;
+  int rval = 0;
+
+  if (verboseMode) {
+    printf ("WR [%02X]: ", reg);
+    for (int i = 0; i < len ; i++) {
+      printf("%02X ", bufp[i]);
+    }
+    printf("\n");
+  }
+
+  for (int i = 0; i < len; i++) 
+  {
+    rval = set_i2c_register(fd, devAddr, reg+i, bufp[i]);
+  }
+  
+  return rval;    
+
+}
+
+/*
+ * @brief  Read generic device register (platform dependent)
+ *
+ * @param  handle    customizable argument. In this examples is used in
+ *                   order to select the correct sensor bus handler.
+ * @param  reg       register to read
+ * @param  bufp      pointer to buffer that store the data read
+ * @param  len       number of consecutive register to read
+ *
+ */
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len)
+{
+  int fd = (int) handle;
+  int rval = 0;
+  /* Read multiple command */
+ 
+  if (verboseMode)
+      printf ("RD [0x%02X]: ", reg);
+  
+  for (int i = 0; i < len; i++) 
+  {
+    rval = get_i2c_register(fd, devAddr, reg+i, bufp+i);
+    if (verboseMode)
+       printf("%02X ", bufp[i]);
+  }
+  if (verboseMode)
+      printf ("\n");
+  
+    
+  return rval;    
+  
+  
+}
+
+/*
+ * @brief  Send buffer to console (platform dependent)
+ *
+ * @param  tx_buffer     buffer to trasmit
+ * @param  len           number of byte to send
+ *
+ */
+static void tx_com(uint8_t *tx_buffer, uint16_t len)
+{
+  printf("%s", tx_buffer);
+}
+
+/*
+ * @brief  platform specific initialization (platform dependent)
+ */
+static int platform_init(char* devPath)
+{
+  int fd;
+  if ((fd = open(devPath, O_RDWR)) < 0)
+  {
+    printf("unable to open I2C: %s\n", strerror(errno));
+    return (-1);
+    
+  }
+  return fd;
+}
+
+
 
 
 static int get_i2c_register(int fd,
@@ -116,218 +427,3 @@ static int set_i2c_register(int fd,
     return 0;
 }
 
-
-int main(int argc, char **argv)
-{
-  /*
-   *  Initialize mems driver interface
-   */
-  	
-   uint8_t addr = ACCEL_ADDR;
-   uint8_t tLow, tHigh;
-   uint8_t reg;
-
-  lis2dh12_ctx_t dev_ctx;
-  int fd;
-
-  dev_ctx.write_reg = platform_write;
-  dev_ctx.read_reg = platform_read;
-  
-  /*
-   * Initialize platform specific hardware
-   */
-  fd = platform_init(addr);
-  if (fd < 0)
-  {
-    printf("unable to open I2C - exit\n");
-    return (-1);
-  }
-
-  dev_ctx.handle = (void*) fd;
- 
-  lis2dh12_device_id_get(&dev_ctx, &whoamI);
-
-  printf("device id: %02X\n", whoamI);
-  if (whoamI != LIS2DH12_ID)
-  {
-    printf ("Device not found\n");
-    
-  } else {
-    printf ("Identifed LIS2DH12...\n");
-  }
-  
-
-  /*
-   *  Enable Block Data Update
-   */
-  lis2dh12_block_data_update_set(&dev_ctx, PROPERTY_ENABLE);
-
-  /*
-   * Set Output Data Rate to 1Hz
-   */
-  lis2dh12_data_rate_set(&dev_ctx, LIS2DH12_ODR_1Hz);
-
-  /*
-   * Set full scale to 2g
-   */
-  lis2dh12_full_scale_set(&dev_ctx, LIS2DH12_2g);
-
-  /*
-   * Enable temperature sensor
-   */
-  lis2dh12_temperature_meas_set(&dev_ctx, LIS2DH12_TEMP_ENABLE);
-
-  /*
-   * Set device in continuous mode with 12 bit resol.
-   */
-  //lis2dh12_operating_mode_set(&dev_ctx, LIS2DH12_LP_8bit);
-  lis2dh12_operating_mode_set(&dev_ctx, LIS2DH12_HR_12bit);
-
-  get_i2c_register(fd, addr, 0x1F, &reg);
-  printf ("temperature register: %02X\n", reg);
-  
-  
-  /*
-   * Read samples in polling mode (no int)
-   */
-  while (1)
-  {
-    lis2dh12_reg_t reg;
-
-    /*
-     * Read output only if new value available
-     */
-    lis2dh12_xl_data_ready_get(&dev_ctx, &reg.byte);
-    if (reg.byte)
-    {
-      /* Read accelerometer data */
-      memset(data_raw_acceleration.u8bit, 0x00, 3 * sizeof(int16_t));
-      lis2dh12_acceleration_raw_get(&dev_ctx, data_raw_acceleration.u8bit);
-      acceleration_mg[0] =
-          lis2dh12_from_fs2_hr_to_mg(data_raw_acceleration.i16bit[0]);
-      acceleration_mg[1] =
-          lis2dh12_from_fs2_hr_to_mg(data_raw_acceleration.i16bit[1]);
-      acceleration_mg[2] =
-          lis2dh12_from_fs2_hr_to_mg(data_raw_acceleration.i16bit[2]);
-
-      sprintf((char *)tx_buffer, "Acceleration [mg]:%4.2f\t%4.2f\t%4.2f\r\n",
-              acceleration_mg[0], acceleration_mg[1], acceleration_mg[2]);
-      tx_com(tx_buffer, strlen((char const *)tx_buffer));
-    }
-
-    lis2dh12_temp_data_ready_get(&dev_ctx, &reg.byte);
-    if (reg.byte)
-    {
-      /* Read temperature data */
-      memset(data_raw_temperature.u8bit, 0x00, sizeof(int16_t));
-      lis2dh12_temperature_raw_get(&dev_ctx, data_raw_temperature.u8bit);
-      temperature_degC =
-          lis2dh12_from_lsb_hr_to_celsius(data_raw_temperature.i16bit);
-
-      sprintf((char *)tx_buffer,
-              "Temperature [degC]:%6.2f\r\n",
-              temperature_degC);
-      tx_com(tx_buffer, strlen((char const *)tx_buffer));
-    }
-     sleep(1);
-    
-    
-    get_i2c_register(fd, addr, LIS2DH12_OUT_TEMP_L, &tLow);
-    get_i2c_register(fd, addr, LIS2DH12_OUT_TEMP_H, &tHigh);
-    printf("tLow=%02X  tHigh=%02X\n", tLow, tHigh);
-    sleep(1);
-  }
-  return (0);
-}
-
-
-
-/*
- * @brief  Write generic device register (platform dependent)
- *
- * @param  handle    customizable argument. In this examples is used in
- *                   order to select the correct sensor bus handler.
- * @param  reg       register to write
- * @param  bufp      pointer to data to write in register reg
- * @param  len       number of consecutive register to write
- *
- */
-static int32_t platform_write(void *handle, uint8_t reg, uint8_t *bufp,
-                              uint16_t len)
-{
-  int fd = (int) handle;
-  int rval = 0;
-
-  printf ("WR [%02X]: ", reg);
-  for (int i = 0; i < len ; i++) {
-    printf("%02X ", bufp[i]);
-  }
-  printf("\n");
-
-
-  for (int i = 0; i < len; i++) 
-  {
-    rval = set_i2c_register(fd, ACCEL_ADDR, reg+i, bufp[i]);
-  }
-  
-  return rval;    
-
-}
-
-/*
- * @brief  Read generic device register (platform dependent)
- *
- * @param  handle    customizable argument. In this examples is used in
- *                   order to select the correct sensor bus handler.
- * @param  reg       register to read
- * @param  bufp      pointer to buffer that store the data read
- * @param  len       number of consecutive register to read
- *
- */
-static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
-                             uint16_t len)
-{
-  int fd = (int) handle;
-  int rval = 0;
-  /* Read multiple command */
- 
-  printf ("RD [0x%02X]: ", reg);
-  for (int i = 0; i < len; i++) 
-  {
-    rval = get_i2c_register(fd, ACCEL_ADDR | 0x80, reg+i, bufp+i);
-    printf("%02X ", bufp[i]);
-  }
-  printf ("\n");
-  
-    
-  return rval;    
-  
-  
-}
-
-/*
- * @brief  Send buffer to console (platform dependent)
- *
- * @param  tx_buffer     buffer to trasmit
- * @param  len           number of byte to send
- *
- */
-static void tx_com(uint8_t *tx_buffer, uint16_t len)
-{
-  printf("%s\n", tx_buffer);
-}
-
-/*
- * @brief  platform specific initialization (platform dependent)
- */
-static int platform_init(int devId)
-{
-  int fd;
-  if ((fd = open(DEV_I2C, O_RDWR)) < 0)
-  {
-    printf("unable to open I2C: %s\n", strerror(errno));
-    return (-1);
-    
-  }
-  return fd;
-}
